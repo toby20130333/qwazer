e95578e110f42799b597ca2a911d10be86005952
Assaf Paz <damagedspline@gmail.com>
2011-08-06 21:25:56 +0300

    Initial Harmattan code re-alignment - needs a few fixes + favorites integration

--- qwazer/main.cpp
+++ qwazer/main.cpp
@@ -9,15 +9,21 @@ int main(int argc, char *argv[])
                         &app, SLOT(quit()));
     QDir qwazerDir = QDir(app.applicationDirPath());
 
+    QString mainQML;
 #ifdef Q_WS_MAEMO_5
     view.engine()->addImportPath(QString("/opt/qtm12/imports"));
     qwazerDir.cdUp();
     view.showMaximized();
+    mainQML = QString("/qml/maemo/main.qml");
+#elif Q_WS_MAEMO_6
+    qwazerDir.cdUp();
+    view.showMaximized();
+    mainQML = QString("/qml/meego/main.qml");
 #else
     view.showNormal();
 #endif
 
-    view.setSource(QUrl(QString(qwazerDir.absolutePath()).append("/qml/maemo/main.qml")));
+    view.setSource(QUrl(QString(qwazerDir.absolutePath()).append(mainQML)));
 
     return app.exec();
 }
